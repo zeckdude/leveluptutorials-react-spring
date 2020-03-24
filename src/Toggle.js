8e2c6722d561fa8b59a50d3c8dd642e7a8be6214
zeckdude <zeckdude@gmail.com>
2020-03-24 00:09:48 -0700

    1 file modified

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,7 +4,10 @@ import { useSpring, animated } from 'react-spring';
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
   const fade = useSpring({
-    opacity: isToggled ? 1 : 0
+    opacity: isToggled ? 1 : 0,
+    fontSize: isToggled ? '2rem' : '5em',
+    //color: isToggled ? 'tomato' : 'green',
+    transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)'
   });
 
   return (
