92c38bcdd18160c7bd68c1ed7d11f4e1b69a0881
zeckdude <zeckdude@gmail.com>
2020-03-25 13:44:54 -0700

    1 file modified

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,15 +4,22 @@ import { useSpring, animated } from 'react-spring';
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
   const { color, y } = useSpring({
+    // opacity: isToggled ? 1 : 0,
+    // fontSize: isToggled ? '2rem' : '5em',
     color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : -50
+    y: isToggled ? 0 : 1
   });
 
   return (
     <div>
       <animated.h1
         style={{
-          transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+          transform: y
+            .interpolate({
+              range: [0, 0.25, 0.5, 0.75, 1],
+              output: [0, -25, -50, -100, -50]
+            })
+            .interpolate(y => `translate3d(0, ${y}px, 0)`),
           color
         }}
       >
