4f3af9efad2b902c7962ae432dd04457c8784374
zeckdude <zeckdude@gmail.com>
2020-03-25 14:12:27 -0700

    1 file modified

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,35 +1,27 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
-  const { color, y } = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    // fontSize: isToggled ? '2rem' : '5em',
-    color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : 1
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   });
 
   return (
     <div>
-      <animated.h1
-        style={{
-          transform: y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50]
-            })
-            .interpolate(y => `translate3d(0, ${y}px, 0)`),
-          color
-        }}
-      >
-        Hello
-      </animated.h1>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.h1 key={key} style={props}>
+              Hello
+            </animated.h1>
+          )
+      )}
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
     </div>
   );
 };
-
-// const AnimatedTitle = animated(componentName);
 
 export default Toggle;
